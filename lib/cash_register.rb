2e9971fd0ef28ada6b101d44ff71bb1c44e71c5c
Ronald Burrell Jr <ronburrell05@gmail.com>
2020-06-18 15:04:09 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,6 +1,6 @@
 class CashRegister
   
-  attr_accessor :total, :discount, :price, :items, :prices
+  attr_accessor :total, :discount, :price, :items, :last_transaction
   
   def initialize(discount = 0)
     @total = 0
